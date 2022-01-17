2c4ea88a0981e0dea822d6dd732b4c390fa5de24
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2022-01-17 14:38:58 +0300

    Add project files.

---
+++ WeatherBot/Handlers.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace WeatherBot
+{
+    public class Handlers
+    {
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = update.Type switch
+            {
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message)
+            };
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message? message)
+        {
+            if (string.IsNullOrWhiteSpace(message.Text))
+                return;
+            if (message.Text.Contains("/hi"))
+            {
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Hello from WeatherBot");
+            }
+        }
+
+        internal static Task HandleErrorAsync(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using WeatherBot;
+
+public class Program
+{
+    private static ITelegramBotClient bot;
+    private static IConfiguration config;
+    public static async Task Main(string[] args)
+    {
+        config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+        var token = config.GetSection("Token").Value;
+        bot = new TelegramBotClient(token);
+        var me = await bot.GetMeAsync();
+
+        /// Start receiving messages
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { } // receive all update types
+        };
+        bot.StartReceiving(
+            Handlers.HandleUpdateAsync,
+            Handlers.HandleErrorAsync,
+            receiverOptions,
+            cancellationToken
+        );
+        Console.WriteLine($"Start listening for {me.Username}");
+        Console.ReadLine();
+    }
+   
+}
