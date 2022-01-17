6a8514fc0e0b3178b80d8ce50147ed97da270cf1
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2022-01-17 18:11:39 +0300

    Started worling with Visual Crossing.

---
+++ WeatherApi/Controllers/WeatherForecast.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecast : Controller
+    {
+        private readonly HttpClient client;
+        public WeatherForecast()
+        {
+            client = new HttpClient();
+        }
+
+        [HttpGet("forecast")]
+        public async Task<IActionResult> GetForecast()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                " https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris?key=AE6D3SXYY5DABUW8YZYJV33X7");
+            var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            return Ok(response);
+        }
+    }
+}
--- WeatherBot/Handlers.cs
+++ WeatherBot/Handlers.cs
@@ -10,18 +10,31 @@ namespace WeatherBot
         {
             var handler = update.Type switch
             {
-                UpdateType.Message => BotOnMessageReceived(botClient, update.Message)
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message!)
             };
         }
 
-        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message? message)
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
         {
             if (string.IsNullOrWhiteSpace(message.Text))
                 return;
-            if (message.Text.Contains("/hi"))
+            var action = message.Text!.Split(' ')[0] switch
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Hello from WeatherBot");
-            }
+                "/hi" => SendMessageAsync(botClient, message),
+                _ => UnknownMessage(botClient, message),
+            };
+            await action;
+        }
+
+        private static async Task SendMessageAsync(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Hello from WeatherBot");            
+        }
+                
+        private static async Task UnknownMessage(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id,
+                $"Hmmm...{Environment.NewLine}I don't know this command. Try to use /help for checking that you entered a right command.");
         }
 
         internal static Task HandleErrorAsync(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
