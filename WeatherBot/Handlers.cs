41f07bfdf5577d8fd16f8fac51d5f9ba728d1c3e
Ilya Klishevich <klishevich.ilya@gmail.com>
2022-01-18 17:53:04 +0300

    Started getting user coordinates.

--- WeatherBot/Handlers.cs
+++ WeatherBot/Handlers.cs
@@ -1,6 +1,8 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Yandex.Geocoder;
 
 namespace WeatherBot
 {
@@ -15,15 +17,45 @@ namespace WeatherBot
         }
 
         private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
-        {
-            if (string.IsNullOrWhiteSpace(message.Text))
-                return;
-            var action = message.Text!.Split(' ')[0] switch
+        {          
+
+            if (message.Location is not null)
             {
-                "/hi" => SendMessageAsync(botClient, message),
-                _ => UnknownMessage(botClient, message),
-            };
-            await action;
+                await GetCustomerCity(botClient, message);               
+            }
+
+            if (string.IsNullOrWhiteSpace(message.Text))
+            {
+                var action = message.Text!.Split(' ')[0] switch
+                {
+                    "/hi" => SendMessageAsync(botClient, message),
+                    "/coordinates" => RequestCustomerCoordinate(botClient, message),
+                    "/location" => GetCustomerCity(botClient, message),
+                    _ => UnknownMessage(botClient, message),
+                };
+                await action;
+            }
+            
+           
+        }
+
+        private static async Task RequestCustomerCoordinate(ITelegramBotClient botClient, Message message)
+        {            
+            var keyboard = new ReplyKeyboardMarkup(new[]
+                 {
+                new KeyboardButton("Share your location, please :)")
+                {
+                    RequestLocation = true
+                   
+                },                
+                
+            });
+            var temp = botClient.SendTextMessageAsync(message.Chat.Id, "Send my location", replyMarkup: keyboard).Result.Location;            
+        }
+
+        private static async Task GetCustomerCity(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, $"You lat: {message.Location!.Latitude} long:{message.Location!.Longitude}");           
         }
 
         private static async Task SendMessageAsync(ITelegramBotClient botClient, Message message)
@@ -32,7 +64,7 @@ namespace WeatherBot
         }
                 
         private static async Task UnknownMessage(ITelegramBotClient botClient, Message message)
-        {
+        {   
             await botClient.SendTextMessageAsync(message.Chat.Id,
                 $"Hmmm...{Environment.NewLine}I don't know this command. Try to use /help for checking that you entered a right command.");
         }
